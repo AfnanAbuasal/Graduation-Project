f5ef2f3aadf62cfa01c656c05c52ca251534e647
Afnan Abo-Asal <afnanalaa49@gmail.com>
2025-12-25 20:32:40 +0200

    Added ExamQuestionRepository and IExamQuestionRepository with methods for retrieving exam questions.

---
+++ Sconce.DAL/Repositories/ExamQuestionRepository.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using Sconce.DAL.Data;
+using Sconce.DAL.Models;
+using Sconce.DAL.Repositories.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sconce.DAL.Repositories
+{
+    public class ExamQuestionRepository : GenericRepository<ExamQuestion>, IExamQuestionRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ExamQuestionRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<ExamQuestion>> GetAllByExamIdAsync(int examId)
+        {
+            return await _context.ExamQuestions
+                .Where(eq => eq.ExamId == examId)
+                .OrderBy(eq => eq.SortOrder)
+                .ToListAsync();
+        }
+
+        public async Task<ExamQuestion?> GetByExamAndQuestionAsync(int examId, int questionId)
+        {
+            return await _context.ExamQuestions
+                .FirstOrDefaultAsync(eq => eq.ExamId == examId && eq.QuestionId == questionId);
+        }
+
+        public async Task<bool> ExistsQuestionInExamAsync(int examId, int questionId, int? excludeId = null)
+        {
+            return await _context.ExamQuestions
+                .AnyAsync(eq => eq.ExamId == examId && eq.QuestionId == questionId && eq.Id != excludeId);
+        }
+
+        public async Task<IEnumerable<ExamQuestion>> GetAllByExamIdWithQuestionAsync(int examId)
+        {
+            return await _context.ExamQuestions
+                .Include(eq => eq.Question)
+                .Where(eq => eq.ExamId == examId)
+                .OrderBy(eq => eq.SortOrder)
+                .ToListAsync();
+        }
+    }
+}
---
+++ Sconce.DAL/Repositories/Interfaces/IExamQuestionRepository.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Sconce.DAL.Models;
+
+namespace Sconce.DAL.Repositories.Interfaces
+{
+    public interface IExamQuestionRepository : IGenericRepository<ExamQuestion>
+    {
+        Task<IEnumerable<ExamQuestion>> GetAllByExamIdAsync(int examId);
+        Task<ExamQuestion?> GetByExamAndQuestionAsync(int examId, int questionId);
+        Task<bool> ExistsQuestionInExamAsync(int examId, int questionId, int? excludeId = null);
+        Task<IEnumerable<ExamQuestion>> GetAllByExamIdWithQuestionAsync(int examId); // Include Question
+    }
+}
